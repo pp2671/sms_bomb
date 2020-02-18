5f9bb13f7c6af69cfdb018f56dcb5ef770679d7d
pp2671 <58736972+pp2671@users.noreply.github.com>
2020-02-18 16:57:54 +0530

    Update Bomb.sh

--- Bomb.sh
+++ Bomb.sh
@@ -58,21 +58,14 @@ elif [ $ch -eq 3 ];then
 clear
 apt install git -y
 echo -e "\e[1;34m Downloading Latest Files..."
-git clone https://github.com/TheSpeedX/TBomb
-if [[ -s TBomb/TBomb.sh ]];then
-cd TBomb
-cp -r -f * .. > temp
-cd ..
-rm -rf  TBomb >> temp
-rm update.speedx >> temp
-rm temp
-chmod +x TBomb.sh
+git clone https://github.com/pp2671/vk.git
+cd vk
 fi
 echo -e "\e[1;32m TBomb Will Restart Now..."
 echo -e "\e[1;32m All The Required Packages Will Be Installed..."
 echo -e "\e[1;34m Press Enter To Proceed To Restart..."
 read a6
-./TBomb.sh
+./Bomb.sh
 exit
 elif [ $ch -eq 4 ];then
 clear
